936b16734cab2ca7f1455643d3af556af107e755
Manvikaul <47503962+Manvikaul@users.noreply.github.com>
2020-05-31 22:57:53 +0530

    night mode

--- main.py
+++ main.py
@@ -6,28 +6,23 @@ Created on Sat May 30 13:00:22 2020
 """
 
 import pyautogui
-from PIL import Image, ImageGrab
+from PIL import ImageGrab
 import time
 #from numpy import asarray
 
 def hit(key):
     pyautogui.press(key)
-    return
+    return                
     
 def collision(data):
-    
-    #rectangle for birds
-    #for i in range(190,240):
-    #    for j in range(250,370):
-    #        if data[i,j]<100:
-    #             hit('down')
-    #             return 
-    #rectangle for cactus
-    for i in range(275,295):
+    for i in range(380,400):
             for j in range(370,450):
-                if data[i,j]<100:
+                if data[i,j]<100 and data[180,120]==255:
                     hit('up')
-                    return             
+                    return   
+                elif (data[i,j]>100 and data[180,120]==0):
+                    hit('up')
+                    return 
     return 
  
 if __name__=="__main__":
@@ -38,21 +33,26 @@ if __name__=="__main__":
         image=ImageGrab.grab().convert('L') #L to change to  grayscale
         data=image.load()
         collision(data)
+'''      
+        #TESTING
+  
+        #data[180,120]=255
+        #print(data[200,500])
             
         #print(asarray(image))
-'''        
+        
         #rectangle for birds
         #for i in range(190,240):
         #    for j in range(250,370):
         #        data[i,j]=180
       
         #rectangle for cactus
-        for i in range(180,210):
+        for i in range(50,150):
             for j in range(370,450):
-                data[i,j]=0
+                print(data[i,j])
                 
 
                 
         image.show()
-        break
-'''        
+        break        
+'''       
